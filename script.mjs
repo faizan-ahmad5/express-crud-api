1577f06f604644028d3a71b3398f95ca917d1f47
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-09-22 09:37:25 +0500

    Implement POST request to add new item

--- script.mjs
+++ script.mjs
@@ -25,11 +25,39 @@ const users = [
   },
 ];
 
-app.get("/", (req, res) => {
+app.get("/api/users", (req, res) => {
   try {
-    res.status(200).json({ message: "Fetching all users from the array", users });
+    res
+      .status(200)
+      .json({ message: "Fetching all users from the array", users });
   } catch (error) {
-    res.status(500).json({ message: "An error occurred", error: error.message });
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
+});
+
+app.post("/api/users", (req, res) => {
+  try {
+    // console.log("Request Body:", req.body);
+    const { id, name, email, age } = req.body;
+
+    if (!id || !name || !email || !age) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const existingUser = users.find((user) => user.id === id);
+    if (existingUser) {
+      return res.status(400).json({ message: "User ID must be unique" });
+    }
+
+    const newUser = { id, name, email, age };
+    users.push(newUser);
+    res.status(201).json({ message: "User added successfully", newUser });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
   }
 });
 
