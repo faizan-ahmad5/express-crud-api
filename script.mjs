5435549893440dac14340729e66ded35302ebf17
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-09-22 08:58:27 +0500

    Add sample items array to simulate data storage

--- script.mjs
+++ script.mjs
@@ -4,6 +4,27 @@ const app = express();
 app.use(express.json());
 const PORT = 3000;
 
+const users = [
+  {
+    id: 1,
+    name: "Faizan Ahmad",
+    email: "faizan@gmail.com",
+    age: 20,
+  },
+  {
+    id: 2,
+    name: "Ahmad Khan",
+    email: "ahmad@gmail.com",
+    age: 22,
+  },
+  {
+    id: 3,
+    name: "Ali",
+    email: "ali@gmail.com",
+    age: 24,
+  },
+];
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
